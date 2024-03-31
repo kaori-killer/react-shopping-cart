1fcaebbc75fb0920da6f0f6317d036508bedf8df
kaori-killer <75800958+kaori-killer@users.noreply.github.com>
2024-04-01 04:30:45 +0900

    상품들은 n x 4 레이아웃으로 보여진다.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,24 @@
 import product from './assets/images/product.png';
 import cart from './assets/svgs/cart.svg';
 
-function App() {
+import Img from './components/Img';
+
+function Product() {
+  return (
+    <div>
+      <Img variant="default" src={product} alt="PET보틀-정사각(420ml)" />
+      <div className="flex justify-between w-280 p-5">
+        <div className="product-info">
+          <span className="product-info__name">PET보틀-정사각(420ml)</span>
+          <span className="product-info__price">43,000원</span>
+        </div>
+        <img src={cart} alt="장바구니" /> 
+      </div>
+    </div>
+  );
+}
+
+export default function App() {
   return (
     <div className="root">
       <nav className="nav flex justify-around">
@@ -13,131 +30,11 @@ function App() {
           <button className="nav-button">주문목록</button>
         </div>
       </nav>
-
       <section className="product-container">
-        <div>
-          <img src={product} alt="PET보틀-정사각(420ml)" />
-          <div className="flex justify-between w-280 p-5">
-            <div className="product-info">
-              <span className="product-info__name">PET보틀-정사각(420ml)</span>
-              <span className="product-info__price">43,000원</span>
-            </div>
-            <img src={cart} alt="장바구니" /> 
-          </div>
-        </div>
-        <div>
-          <img src={product} alt="PET보틀-정사각(420ml)" />
-          <div className="flex justify-between w-280 p-5">
-            <div className="product-info">
-              <span className="product-info__name">PET보틀-정사각(420ml)</span>
-              <span className="product-info__price">43,000원</span>
-            </div>
-            <img src={cart} alt="장바구니" />
-          </div>
-        </div>
-        <div>
-          <img src={product} alt="PET보틀-정사각(420ml)" />
-          <div className="flex justify-between w-280 p-5">
-            <div className="product-info">
-              <span className="product-info__name">PET보틀-정사각(420ml)</span>
-              <span className="product-info__price">43,000원</span>
-            </div>
-            <img src={cart} alt="장바구니" />
-          </div>
-        </div>
-        <div>
-          <img src={product} alt="PET보틀-정사각(420ml)" />
-          <div className="flex justify-between w-280 p-5">
-            <div className="product-info">
-              <span className="product-info__name">PET보틀-정사각(420ml)</span>
-              <span className="product-info__price">43,000원</span>
-            </div>
-            <img src={cart} alt="장바구니" />
-          </div>
-        </div>
-        <div>
-          <img src={product} alt="PET보틀-정사각(420ml)" />
-          <div className="flex justify-between w-280 p-5">
-            <div className="product-info">
-              <span className="product-info__name">PET보틀-정사각(420ml)</span>
-              <span className="product-info__price">43,000원</span>
-            </div>
-            <img src={cart} alt="장바구니" />
-          </div>
-        </div>
-        <div>
-          <img src={product} alt="PET보틀-정사각(420ml)" />
-          <div className="flex justify-between w-280 p-5">
-            <div className="product-info">
-              <span className="product-info__name">PET보틀-정사각(420ml)</span>
-              <span className="product-info__price">43,000원</span>
-            </div>
-            <img src={cart} alt="장바구니" />
-          </div>
-        </div>
-        <div>
-          <img src={product} alt="PET보틀-정사각(420ml)" />
-          <div className="flex justify-between w-280 p-5">
-            <div className="product-info">
-              <span className="product-info__name">PET보틀-정사각(420ml)</span>
-              <span className="product-info__price">43,000원</span>
-            </div>
-            <img src={cart} alt="장바구니" />
-          </div>
-        </div>
-        <div>
-          <img src={product} alt="PET보틀-정사각(420ml)" />
-          <div className="flex justify-between w-280 p-5">
-            <div className="product-info">
-              <span className="product-info__name">PET보틀-정사각(420ml)</span>
-              <span className="product-info__price">43,000원</span>
-            </div>
-            <img src={cart} alt="장바구니" />
-          </div>
-        </div>
-        <div>
-          <img src={product} alt="PET보틀-정사각(420ml)" />
-          <div className="flex justify-between w-280 p-5">
-            <div className="product-info">
-              <span className="product-info__name">PET보틀-정사각(420ml)</span>
-              <span className="product-info__price">43,000원</span>
-            </div>
-            <img src={cart} alt="장바구니" />
-          </div>
-        </div>
-        <div>
-          <img src={product} alt="PET보틀-정사각(420ml)" />
-          <div className="flex justify-between w-280 p-5">
-            <div className="product-info">
-              <span className="product-info__name">PET보틀-정사각(420ml)</span>
-              <span className="product-info__price">43,000원</span>
-            </div>
-            <img src={cart} alt="장바구니" />
-          </div>
-        </div>
-        <div>
-          <img src={product} alt="PET보틀-정사각(420ml)" />
-          <div className="flex justify-between w-280 p-5">
-            <div className="product-info">
-              <span className="product-info__name">PET보틀-정사각(420ml)</span>
-              <span className="product-info__price">43,000원</span>
-            </div>
-            <img src={cart} alt="장바구니" />
-          </div>
-        </div>
-        <div>
-          <img src={product} alt="PET보틀-정사각(420ml)" />
-          <div className="flex justify-between w-280 p-5">
-            <div className="product-info">
-              <span className="product-info__name">PET보틀-정사각(420ml)</span>
-              <span className="product-info__price">43,000원</span>
-            </div>
-            <img src={cart} alt="장바구니" />
-          </div>
-        </div>
+        {new Array(16).fill(0).map((_, i) => (
+          <Product key={i} />
+        ))}
       </section>
     </div>
   )
 }
-
-export default App
---
+++ frontend/src/components/Img.tsx
@@ -0,0 +1,17 @@
+const size = {
+    default: '', 
+    small: 'w-144 h-144',
+    big: 'w-480 h-480',
+}
+
+type ImgProps = {
+    variant: 'default' | 'small' | 'big';
+    src: string;
+    alt: string;
+};
+
+export default function Img({variant, src, alt}: ImgProps){
+    return (
+        <img className={size[variant]} src={src} alt={alt} />
+    );
+}
