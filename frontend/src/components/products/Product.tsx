3a1591346ec9c9f6f3e8ce3ddcc3b557135d9f86
kaori-killer <75800958+kaori-killer@users.noreply.github.com>
2024-04-01 14:32:56 +0900

    메인 페이지 장바구니 버튼 클릭 시 체크 박스 모양으로 변환

--- frontend/src/components/products/Product.tsx
+++ frontend/src/components/products/Product.tsx
@@ -1,9 +1,17 @@
+import { useBoolean } from "usehooks-ts";
+
 import product from "../../assets/images/product.png";
 import cart from "../../assets/svgs/cart.svg";
 
 import Img from "../Img";
 
 export default function Product() {
+  const { value: isSelect, toggle } = useBoolean(false);
+
+  const handleClickSelect = () => {
+    toggle();
+  };
+
   return (
     <div>
       <Img variant="default" src={product} alt="PET보틀-정사각(420ml)" />
@@ -12,7 +20,11 @@ export default function Product() {
           <span className="product-info__name">PET보틀-정사각(420ml)</span>
           <span className="product-info__price">43,000원</span>
         </div>
-        <img src={cart} alt="장바구니" />
+        {isSelect ? (
+          <p>✅</p>
+        ) : (
+          <img src={cart} alt="장바구니" onClick={handleClickSelect} />
+        )}
       </div>
     </div>
   );
