fec8ed43e1366bdfb123c258645b6a8d8c2be9a8
kaori-killer <75800958+kaori-killer@users.noreply.github.com>
2024-04-01 14:13:33 +0900

    헤더에 있는 버튼을 누르면 그에 맞는 페이지로 이동한다

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,12 +1,33 @@
+import { useNavigate } from "react-router-dom";
+
 export default function Header() {
+  const navigate = useNavigate();
+
+  const handleClickHome = () => {
+    navigate("/");
+  };
+  const handleClickCart = () => {
+    navigate("/cart");
+  };
+
+  const handleClickOrder = () => {
+    navigate("/order");
+  };
+
   return (
     <nav className="nav flex justify-around">
       <div className="flex-center">
-        <h1 className="nav-title">CLEAN CODE SHOP</h1>
+        <h1 className="nav-title" onClick={handleClickHome}>
+          CLEAN CODE SHOP
+        </h1>
       </div>
       <div className="flex gap-15">
-        <button className="nav-button">장바구니</button>
-        <button className="nav-button">주문목록</button>
+        <button className="nav-button" onClick={handleClickCart}>
+          장바구니
+        </button>
+        <button className="nav-button" onClick={handleClickOrder}>
+          주문목록
+        </button>
       </div>
     </nav>
   );
