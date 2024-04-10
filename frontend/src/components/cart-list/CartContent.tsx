94c0e7032562b8cb4a478b95c36dfe8660504a97
kaori-killer <75800958+kaori-killer@users.noreply.github.com>
2024-04-10 16:00:39 +0900

    장바구니에서 수량을 증가/감소 시킬 수 있다

--- frontend/src/components/cart-list/CartContent.tsx
+++ frontend/src/components/cart-list/CartContent.tsx
@@ -17,12 +17,24 @@ type QuantityProps = {
 };
 
 function Quantity({ quantity, updateQuantity }: QuantityProps) {
+  const handleClickIncrease = () => {
+    updateQuantity(quantity + 1);
+  };
+
+  const handleClickDecrease = () => {
+    updateQuantity(quantity - 1);
+  };
+
   return (
     <div className="number-input-container">
       <input type="number" className="number-input" value={quantity} />
       <div>
-        <button className="number-input-button">▲</button>
-        <button className="number-input-button">▼</button>
+        <button className="number-input-button" onClick={handleClickIncrease}>
+          ▲
+        </button>
+        <button className="number-input-button" onClick={handleClickDecrease}>
+          ▼
+        </button>
       </div>
     </div>
   );
