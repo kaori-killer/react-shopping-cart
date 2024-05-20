799cb9eb8797bb3ad78d08c1e061ad8f334cff5c
kaori-killer <75800958+kaori-killer@users.noreply.github.com>
2024-05-21 00:48:43 +0900

    자식 요소가 없는 태그는 self closing tag로 변경

--- frontend/src/pages/OrderPayListPage.tsx
+++ frontend/src/pages/OrderPayListPage.tsx
@@ -5,7 +5,7 @@ import OrderPaySection from "../components/order-pay-list/OrderPaySection";
 export default function OrderPayListPage() {
   return (
     <section className="order-section">
-      <Title variant="order" text="주문/결제"></Title>
+      <Title variant="order" text="주문/결제" />
       <div className="flex">
         <OrderPayList />
         <OrderPaySection />
